63ce03184ed9f3014f155e90d9bb1448396d566c
darrior <andrei150202@gmail.com>
2022-10-06 02:38:00 +0300

    Implement `encrypt_array`

--- magma-cipher/src/lib.rs
+++ magma-cipher/src/lib.rs
@@ -1,5 +1,9 @@
 use hex_literal::hex;
-use magma::cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit};
+use magma::cipher::generic_array::ArrayLength;
+use magma::cipher::{
+    generic_array::{typenum::U8, GenericArray},
+    BlockDecrypt, BlockEncrypt, KeyInit,
+};
 use magma::Magma;
 use rand::prelude::*;
 
@@ -25,7 +29,20 @@ fn generate_key() -> Vec<u8> {
 }
 
 fn encrypt_array(arr: Vec<u8>, key: Vec<u8>) -> Vec<u8> {
-    todo!()
+    let cipher = Magma::new(key.as_slice().into());
+    let mut arr = arr;
+    for i in 0..(arr.len() % 8) {
+        arr.push(0);
+    }
+    let mut chunks: Vec<&[u8]> = arr.chunks(8).collect();
+    let mut ret = Vec::new();
+    for i in chunks.iter() {
+        let mut block = GenericArray::clone_from_slice(*i);
+	cipher.encrypt_block(&mut block);
+	let mut block = block.to_vec();
+	ret.append(&mut block);
+    }
+    ret
 }
 
 fn decrypt_array(arr: Vec<u8>, key: Vec<u8>) -> Vec<u8> {
@@ -48,10 +65,10 @@ mod tests {
     fn test_encrypt_array() {
         let key = hex!("FFEEDDCCBBAA99887766554433221100"
 		       "F0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF");
-	let arr = hex!("FEDCBA9876543210");
-	let ciph_arr : Vec<u8> = hex!("4EE901E5C2D8CA3D").into();
-	
-	let arr = encrypt_array(arr.into(), key.into());
-	assert_eq!(arr, ciph_arr);
+        let arr = hex!("FEDCBA9876543210");
+        let ciph_arr: Vec<u8> = hex!("4EE901E5C2D8CA3D").into();
+
+        let arr = encrypt_array(arr.into(), key.into());
+        assert_eq!(arr, ciph_arr);
     }
 }
