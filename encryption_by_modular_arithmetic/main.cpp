e93a4eb3158bf9774df9e63f419e22d7799fdf75
Ilya Eroshev <eroshev.il@yandex.ru>
2022-10-05 17:25:40 +0300

    Add an implementation for binary exponentiation

--- encryption_by_modular_arithmetic/main.cpp
+++ encryption_by_modular_arithmetic/main.cpp
@@ -1,6 +1,13 @@
 namespace auxiliary_functions {
-	uint64_t pow(uint64_t, uint64_t) {
-		return 0;
+	uint64_t pow(uint64_t b, uint64_t e) {//b^e по модулю 2^64
+		uint64_t res = (e & 1) ? b : 1;
+		e >>= 1;
+		while(e) {
+			b *= b;
+			if(e & 1) res *= b;
+			e >>= 1;
+		}
+		return res;
 	}
 }
 
