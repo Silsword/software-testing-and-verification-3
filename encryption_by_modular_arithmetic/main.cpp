7cd05e3401d2ab2398796f7ef194955339ea3e70
Ilya Eroshev <eroshev.il@yandex.ru>
2022-10-06 13:05:55 +0300

    Add an implementation for encrypting/decrypting files

--- encryption_by_modular_arithmetic/main.cpp
+++ encryption_by_modular_arithmetic/main.cpp
@@ -34,14 +34,101 @@ namespace auxiliary_functions {
 	}
 }
 
+void generate_key_to_file (const char *key_file_name) {
+	std::ofstream out(key_file_name, std::ios::trunc);
+	uint64_t shift = auxiliary_functions::random_key();
+	uint64_t multiplier = auxiliary_functions::random_key();
+	out << shift << ' ' << multiplier;
+}
+
 void encrypt(const char *input_file_name, const char *key_file_name) {
+	using namespace std;
+	ifstream key_file(key_file_name);
+	uint64_t shift, multiplier;
+	key_file >> shift >> multiplier;
+	key_file.close();
+
+	ifstream in(input_file_name, ios::binary);
+	string output_file_name = input_file_name + ".enc"s;
+	ofstream out(output_file_name, ios::binary);
+
+	uint64_t buf = 0;
+	int i = 0;
+	char c;
+	while(in.get(c)) {
+		buf <<= 8;
+		buf += static_cast<uint8_t>(c);
+		++i;
+		if(i == 8) {
+			auxiliary_functions::print_as_characters(out, (buf + shift) * multiplier);
+			buf = 0;
+			i = 0;
+		}
+	}
+	if (i != 0) {
+		buf <<= 8 * (8 - i);
+		auxiliary_functions::print_as_characters(out, (buf + shift) * multiplier);
+	}
+	in.close();
+	out.close();
 }
 
 void encrypt(const char *input_file_name) {//ключ по умолчанию
+	using namespace std;
+	const char *key_file_name = ".default_key";
+	ofstream key_file(key_file_name, ios::trunc);
+	const uint64_t shift = 567342345623ull;
+	const uint64_t multiplier = 1234353354349ull;
+	key_file << shift << ' ' << multiplier;
+	key_file.close();
+
+	encrypt(input_file_name, key_file_name);
+	remove(key_file_name);
 }
 
 void decrypt(const char *input_file_name, const char *key_file_name) {
+	using namespace std;
+	ifstream key_file(key_file_name);
+	uint64_t shift, multiplier;
+	key_file >> shift >> multiplier;
+	key_file.close();
+	multiplier = auxiliary_functions::inv(multiplier);
+
+
+	ifstream in(input_file_name, ios::binary);
+	string output_file_name = input_file_name + ".dec"s;
+	ofstream out(output_file_name, ios::binary);
+
+	uint64_t buf = 0;
+	int i = 0;
+	char c;
+	while(in.get(c)) {
+		buf <<= 8;
+		buf += static_cast<uint8_t>(c);
+		++i;
+		if(i == 8) {
+			auxiliary_functions::print_as_characters(out, buf * multiplier - shift);
+			buf = 0;
+			i = 0;
+		}
+	}
+	if (i != 0) {
+		buf <<= 8 * (8 - i);
+		auxiliary_functions::print_as_characters(out, buf * multiplier - shift);
+	}
+	in.close();
+	out.close();
 }
 
 void decrypt(const char *input_file_name) {//ключ по умолчанию
+	using namespace std;
+	const char *key_file_name = ".default_key";
+	ofstream key_file(key_file_name, ios::trunc);
+	const uint64_t shift = 567342345623ull;
+	const uint64_t multiplier = 1234353354349ull;
+	key_file << shift << ' ' << multiplier;
+	key_file.close();
+
+	decrypt(input_file_name, key_file_name);
+	remove(key_file_name);
 }
